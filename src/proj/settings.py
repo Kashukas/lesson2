33a3ad0265df40d8c1540572863f90dabfdbabe3
Kashuk <spitfa9@gmail.com>
2021-06-04 01:29:06 +0300

    add in settings static_root

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = '/home/Kashuk/lesson2/static'
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
